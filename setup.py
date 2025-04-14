4b443e45ef0cbc9e6add0a14cbc92497750bffd2
Fayssal El Mofatiche <moufatich@gmail.com>
2025-04-15 00:04:11 +0200

    new version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="todo-cli",
-    version="0.1.0",
+    name="flowistic-todo",
+    version="0.1.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -23,7 +23,7 @@ setup(
     long_description_content_type="text/markdown",
     keywords="todo, cli, project management",
     python_requires=">=3.7",
-    url="https://github.com/yourusername/todo-cli",
+    url="https://github.com/flowistic-ai/todo",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
