e4c3d3f7ad537ac8bb81d5652a585473fdad4b8a
Fayssal El Mofatiche (Extern) <fayssal.elmofatiche.ext@lanxess.com>
2025-04-18 16:56:04 +0200

    update setup and uv dependencies

--- setup.py
+++ setup.py
@@ -1,28 +1,33 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+VERSION = "0.1.1"
+
+INSTALL_REQUIRES = [
+    "typer==0.9.0",
+    "rich==13.7.0",
+    "pyyaml==6.0.1",
+    "dateparser==1.2.0",
+]
 
 setup(
     name="todo",
-    version="0.1.1",
+    version=VERSION,
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        "typer==0.9.0",
-        "rich==13.7.0",
-        "pyyaml==6.0.1",
-        "dateparser==1.2.0",
-    ],
+    install_requires=INSTALL_REQUIRES,
     entry_points={
         "console_scripts": [
-            "todo=todo_package.cli:app",
+            "todo=todo.cli:app",
         ],
     },
     author="Fayssal El Mofatiche",
     author_email="fayssal.elmofatiche@flowistic.ai",
     description="A rich CLI todo app with project management and task tagging",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     keywords="todo, cli, project management",
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     url="https://github.com/flowistic-ai/todo",
     classifiers=[
         "Development Status :: 4 - Beta",
